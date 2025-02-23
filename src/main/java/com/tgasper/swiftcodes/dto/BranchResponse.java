d77c50b427af48499e931a17b5cd76bf501ad49d
tomek <tomgasper01@gmail.com>
2025-02-23 15:46:50 +0100

    Add getters and setters for BranchResponse DTO

--- src/main/java/com/tgasper/swiftcodes/dto/BranchResponse.java
+++ src/main/java/com/tgasper/swiftcodes/dto/BranchResponse.java
@@ -16,4 +16,45 @@ public class BranchResponse {
         this.isHeadquarter = swiftCode.isHeadquarter();
         this.swiftCode = swiftCode.getSwiftCode();
     }
+
+    // Getters and Setters
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getCountryISO2() {
+        return countryISO2;
+    }
+
+    public void setCountryISO2(String countryISO2) {
+        this.countryISO2 = countryISO2;
+    }
+
+    public boolean isHeadquarter() {
+        return isHeadquarter;
+    }
+
+    public void setHeadquarter(boolean headquarter) {
+        isHeadquarter = headquarter;
+    }
+
+    public String getSwiftCode() {
+        return swiftCode;
+    }
+
+    public void setSwiftCode(String swiftCode) {
+        this.swiftCode = swiftCode;
+    }
 }
