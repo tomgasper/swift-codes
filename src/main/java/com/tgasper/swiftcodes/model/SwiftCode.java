07a553aea9b9388d99e81a4bf77201eae90aaf04
tomek <tomgasper01@gmail.com>
2025-02-23 12:33:22 +0100

    Add Swift Code model

---
+++ src/main/java/com/tgasper/swiftcodes/model/SwiftCode.java
@@ -0,0 +1,72 @@
+package com.tgasper.swiftcodes.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "swift_codes")
+public class SwiftCode {
+    @Id
+    @Column(name = "swift_code")
+    @NotNull
+    @Size(max = 11)
+    @Pattern(regexp = "^[A-Z]{6}[A-Z0-9]{2}([A-Z0-9]{3})?$", message = "Must be in BIC8 or BIC11 format")
+    private String swiftCode;
+
+    @OneToOne
+    @JoinColumn(name = "bank_id", nullable = false)
+    private Bank bank;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "is_headquarter")
+    private boolean isHeadquarter;
+
+    @ManyToOne
+    @JoinColumn(name = "country_iso2_code", nullable = false)
+    private Country country;
+
+    // Getters and Setters
+    public String getSwiftCode() {
+        return swiftCode;
+    }
+
+    public void setSwiftCode(String swiftCode) {
+        this.swiftCode = swiftCode;
+    }
+
+    public Bank getBank() {
+        return bank;
+    }
+
+    public void setBank(Bank bank) {
+        this.bank = bank;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public boolean isHeadquarter() {
+        return isHeadquarter;
+    }
+
+    public void setHeadquarter(boolean headquarter) {
+        isHeadquarter = headquarter;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+}
