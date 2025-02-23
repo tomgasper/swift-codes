95f5de0fbc17dfa3387c3b1a6bfaa6b638feb147
tomek <tomgasper01@gmail.com>
2025-02-23 11:51:12 +0100

    Add initial project dependencies and configuration

---
+++ src/main/java/com/tgasper/swiftcodes/SwiftCodesApplication.java
@@ -0,0 +1,11 @@
+package com.tgasper.swiftcodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftCodesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SwiftCodesApplication.class, args);
+    }
+}
